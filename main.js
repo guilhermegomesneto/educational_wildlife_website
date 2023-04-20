fd8f9dda5366a9d320662dab8f2545b6d968cb10
Guilherme Gomes Neto <88989905+guilhermegomesneto@users.noreply.github.com>
2023-04-20 11:48:49 -0300

    Creating the environment issues page

---
+++ main.js
@@ -0,0 +1,13 @@
+//Swiper
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
